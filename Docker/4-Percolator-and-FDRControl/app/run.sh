2a369c280d58bee3dda861da0112b25730c80d23
Hyeonuk Shin <shin@hyeon.uk>
2026-03-13 23:43:05 +0900

    feat: Add running fdr_control.py

--- Docker/4-Percolator-and-FDRControl/app/run.sh
+++ Docker/4-Percolator-and-FDRControl/app/run.sh
@@ -4,3 +4,10 @@ percolator t_d.pin \
     -M output/out.decoy \
     -w output/out.weight \
     -Y -U -V SA
+
+python3 fdr_control.py \
+    --fdr_type $FDR_TYPE \
+    --target_path output/out.target \
+    --decoy_path output/out.decoy \
+    --fdr_rate $FDR_RATE \
+    --output_dir output
